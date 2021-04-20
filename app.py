134639bfefc944d1e2a1c72823d8a626b0489c76
carohdez <hdez.carolina@gmail.com>
2021-04-20 16:04:46 +0200

    Corrections intention detection

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ from sqlalchemy.orm import sessionmaker, session
 from sqlalchemy import func
 from sqlalchemy import desc
 from flask_sqlalchemy import SQLAlchemy
+import traceback
+
 db = SQLAlchemy()
 
 logger = logging.getLogger('app')
@@ -30,7 +32,7 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
 
 bp = Blueprint('main', __name__)
 app.register_blueprint(bp)
@@ -50,10 +52,13 @@ entities_endpoint = os.environ['ENTITIES_ENDPOINT']
 aspect_endpoint = os.environ['ASPECT_ENDPOINT']
 subjective_endpoint = os.environ['SUBJECTIVE_ENDPOINT']
 comparison_endpoint = os.environ['COMPARISON_ENDPOINT']
+detail_endpoint = os.environ['DETAIL_ENDPOINT']
 aws_access_key = os.environ['AWS_ACCESS_KEY']
 aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 aws_region = os.environ['AWS_REGION']
 aws_service = os.environ['AWS_SERVICE']
+
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 # Heroku API REST authentication
 user_gui = os.environ['USER_GUI']
@@ -68,12 +73,12 @@ entities_aws_host = entities_endpoint.split('/')[2]
 aspect_aws_host = aspect_endpoint.split('/')[2]
 subjective_aws_host = subjective_endpoint.split('/')[2]
 comparison_aws_host = comparison_endpoint.split('/')[2]
-
+detail_aws_host = detail_endpoint.split('/')[2]
 # with app.app_context():
 #     db.session.rollback()
 #     from .main import routes  # Import routes
 
-#wooz_sentences = pd.read_csv('C:\Python\ConversationalRS\data\WoOzSentences.csv')#to check accuracy on WoOz dataset
+wooz_sentences = pd.read_csv('C:\Python\ConversationalRS\data\WoOzSentences.csv', sep='\t')#to check accuracy on WoOz dataset
 @app.errorhandler(404)
 def not_found(error):
     print(error)
@@ -100,102 +105,143 @@ def get_intention():
     if not request.json or not 'sentence' in request.json:
         abort(400)
     sentence = request.json['sentence']
-    #for i, row in wooz_sentences.iterrows(): #to check accuracy on WoOz dataset
-    #sentence = row['Question']
-    entities_new = []
-    scope = assessment = detail = comparison = ""
+    if len(sentence.split()) < 3:
+        abort(400)
+    for i, row in wooz_sentences.iterrows(): #to check accuracy on WoOz dataset
+        sentence = row['Question']
+        entities_new = []
+        error = "" # TODO: Set an appropiate code to respond insufficient number of words to understand question
+        scope = assessment = detail = comparison = ""
 
-    # entity1 = request.json['entity1']
-    # entity2 = request.json['entity2']
-    # feature1 = request.json['feature1']
-    # feature2 = request.json['feature2']
+        # entity1 = request.json['entity1']
+        # entity2 = request.json['entity2']
+        # feature1 = request.json['feature1']
+        # feature2 = request.json['feature2']
 
-    try:
-        # Get entities
-        auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
-                               aws_host=entities_aws_host,aws_region=aws_region, aws_service=aws_service)
-        response = requests.post(entities_endpoint,json={"sentence": sentence},auth=auth)
-        if not response: #or not 'hotels' in request.json:
-            abort(400)
-        if 'error' in response.json():
-            print("Error getting entities:"+response.json().get("error"))
-        if 'hotels' in response.json():
-            hotels = response.json().get("hotels")
-            for i in range(len(hotels)):
-                entities_new.append(hotels[i])
-        print(entities_new)
-        # Get features
-        #TODO
-        # Get scope (possible values: Single, Tuple, Indefinite
-        if len(entities_new) == 1:
-            scope="single"
-        elif len(entities_new) == 0:
-            scope="indefinite"
-        else:
-            scope="tuple"
+        try:
+            # Pre processing: #TODO: splitting, lower upper case?
+            # greetings and thank you
 
-        # Get comparison
-        comparison = ""
-        auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
-                               aws_host=comparison_aws_host,aws_region=aws_region, aws_service=aws_service)
-        response = requests.post(comparison_endpoint, json={"sentence": sentence},auth=auth)
-        if not response:  # or not 'hotels' in request.json:
-            abort(400)
-        if 'error' in response.json():
-            print("Error getting comparison:" + response.json().get("error"))
-        if 'type' in response.json():
-            comparison = response.json().get("type")
-        else:
-            comparison="not_found"
-
-        # Get assessment
-        assessment = ""
-        auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
-                               aws_host=subjective_aws_host,aws_region=aws_region, aws_service=aws_service)
-        response = requests.post(subjective_endpoint, json={"sentence": sentence},auth=auth)
-        if not response:  # or not 'hotels' in request.json:
-            abort(400)
-        if 'error' in response.json():
-            print("Error getting assessment:" + response.json().get("error"))
-        if 'type' in response.json():
-            type = response.json().get("type")
-            if type == "non_subjective":
-                assessment = "factoid"
+            # Get entities
+            auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
+                                   aws_host=entities_aws_host,aws_region=aws_region, aws_service=aws_service)
+            response = requests.post(entities_endpoint,json={"sentence": sentence},auth=auth)
+            if not response: #or not 'hotels' in request.json:
+                abort(400)
+            if 'error' in response.json():
+                print("Error getting entities:"+response.json().get("error"))
+            if 'hotels' in response.json():
+                hotels = response.json().get("hotels")
+                if len(hotels) > 0:
+                    # questions too short: e.g. Hotel Riley. How about hotel Riley.
+                    if hotels[0] in ['PERSON', 'GPE']:
+                        error = "question_needs_clarification"
+                        jsonify({'error': error})
+                for i in range(len(hotels)):
+                    entities_new.append(hotels[i])
+            # Get features
+            #TODO
+            # Get scope (possible values: Single, Tuple, Indefinite
+            if len(entities_new) == 1:
+                scope="single"
+            elif len(entities_new) == 0:
+                scope="indefinite"
             else:
+                scope="tuple"
+
+            # Get comparison
+            comparison = ""
+            sentence = sentence.replace('?','') # workaround, superlative questions are not recognized as superlative when question mark included.
+            auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
+                                   aws_host=comparison_aws_host,aws_region=aws_region, aws_service=aws_service)
+            response = requests.post(comparison_endpoint, json={"sentence": sentence},auth=auth)
+            if not response:  # or not 'hotels' in request.json:
+                abort(400)
+            if 'error' in response.json():
+                print("Error getting comparison:" + response.json().get("error"))
+            if 'type' in response.json():
+                comparison = response.json().get("type")
+            else:
+                comparison = "not_found"
+            if comparison == 'non_comparative':
+                if ("which" in sentence.lower()) & (("most" in sentence.lower()) | ("nearest" in sentence.lower())): # Workaround, special cases not recognized as comparative
+                    comparison = "comparative"
+
+
+
+            # Get assessment
+            assessment = ""
+            sentence_split = sentence.split(" ")
+            if ('free' in sentence.lower()) | ('complimentary' in sentence.lower()):
+                assessment = "factoid"
+            elif ('how are' in sentence.lower()) | ('how is' in sentence.lower()):
                 assessment = "subjective"
-        else:
-            assessment = "not_found"
-        if "why" in sentence.lower():
-            assessment = "why-recommended"
+            else:
+                auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
+                                       aws_host=subjective_aws_host,aws_region=aws_region, aws_service=aws_service)
+                response = requests.post(subjective_endpoint, json={"sentence": sentence},auth=auth)
+                if not response:  # or not 'hotels' in request.json:
+                    abort(400)
+                if 'error' in response.json():
+                    print("Error getting assessment:" + response.json().get("error"))
+                if 'type' in response.json():
+                    type = response.json().get("type")
+                    if type == "non_subjective":
+                        assessment = "factoid"
+                    else:
+                        assessment = "subjective"
+                else:
+                    assessment = "not_found"
+            if "why" in sentence.lower():
+                assessment = "why-recommended"
 
-        # Get Detail
-        detail = ""
-        auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
-                               aws_host=aspect_aws_host,aws_region=aws_region, aws_service=aws_service)
-        response = requests.post(aspect_endpoint, json={"sentence": sentence},auth=auth)
-        if not response:  # or not 'hotels' in request.json:
-            abort(400)
-        if 'error' in response.json():
-            print("Error getting detail:" + response.json().get("error"))
-        if 'aspect' in response.json():
-            detail = response.json().get("aspect")
-        else:
-            detail = "not_found"
+            # if comparative, by default assessment subjective
+            if (comparison == 'comparative') & (assessment == 'factoid'):
+                assessment = 'subjective'
 
-        # TODO: Handle time out of aws services
-        entity1 = entity2 = aspect1 = aspect2 = "" #TODO: handle entities and aspects
-        #intention = [{'scope':scope, 'assessment':assessment, 'detail':detail, 'entity1':"", 'entity2':"", 'feature1':"", 'feature2':"" }]
-        intention = [{'scope': scope, 'assessment': assessment, 'detail': detail, 'comparison': comparison}]
+            # Get Detail
+            detail = ""
+            auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
+                                   aws_host=detail_aws_host,aws_region=aws_region, aws_service=aws_service)
+            response = requests.post(detail_endpoint, json={"sentence": sentence},auth=auth)
+            if not response:  # or not 'hotels' in request.json:
+                abort(400)
+            if 'error' in response.json():
+                print("Error getting detail:" + response.json().get("error"))
+            if 'detail' in response.json():
+                detail = response.json().get("detail")
+            else:
+                detail = "not_found"
+            # Get Aspect
+            aspect = ""
+            if detail == 'aspect':
+                auth = AWSRequestsAuth(aws_access_key=aws_access_key, aws_secret_access_key=aws_secret_access_key,
+                                       aws_host=aspect_aws_host, aws_region=aws_region, aws_service=aws_service)
+                response = requests.post(aspect_endpoint, json={"sentence": sentence}, auth=auth)
+                if not response:  # or not 'hotels' in request.json:
+                    abort(400)
+                if 'error' in response.json():
+                    print("Error getting detail:" + response.json().get("error"))
+                if 'aspect' in response.json():
+                    aspect = response.json().get("aspect")
+                else:
+                    aspect = "not_found"
 
-        print(sentence + str(entities_new) + str(intention))
-    except Exception as e:
-        print('Error Processing: ' + str(e))
-        intention=[]
+            # TODO: Handle time out of aws services
+            entity1 = entity2 = aspect1 = aspect2 = "" #TODO: handle entities and aspects
+            #intention = [{'scope':scope, 'assessment':assessment, 'detail':detail, 'entity1':"", 'entity2':"", 'feature1':"", 'feature2':"" }]
+            intention = [{'scope': scope, 'assessment': assessment, 'detail': detail, 'comparison': comparison}]
+
+            print(sentence + str(entities_new) + str(intention) + " " + str(aspect))
+        except Exception as e:
+            print('Error Processing: ' + str(e))
+            traceback.print_exc()
+            intention=[]
 
     #TODO: handle not finding an intention
     if len(intention) == 0:
        abort(404)
-    return jsonify({'intention': intention[0], 'entities_new': entities_new})
+    return jsonify({'intention': intention[0], 'entities_new': entities_new, 'aspect': aspect})
 
 @app.route('/ExplainableRS/GetReply/', methods=['GET'])
 def get_reply():
